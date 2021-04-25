47d3d034552be2a75f84247a101ff1b6f574dc90
lsras <lsras@DESKTOP-FS8LRFV>
2021-04-24 20:39:24 -0500

    FIRST COMMIT

---
+++ src/test_vehiculo/DemasiadoRapido.java
@@ -0,0 +1,10 @@
+package test_vehiculo;
+
+public class DemasiadoRapido {
+
+    public void MuyRapido(double Velocidad) {
+        if (Velocidad > 100) {
+            System.out.println("Su vehiculo va muy rapido!!!");
+        }
+    }
+}
